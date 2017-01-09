ef4989ecae820a5f5bdc874ce73b769dea4fe725
Ben Sarrazin <ben@berzerker.io>
2017-01-09 16:24:21 -0500

    Basic application skeleton

---
+++ Firebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Firebase
+//
+//  Created by Ben Sarrazin on 2017-01-09.
+//  Copyright © 2017 Berzerker IO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
